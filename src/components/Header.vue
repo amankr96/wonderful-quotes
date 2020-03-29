62e1238235f9d8feb0ce14382d90138a257a9658
amankr96 <aman.kr96@gmail.com>
2020-03-29 14:34:55 +0530

    Changed the progress bar styling

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,8 @@
             <h3>Quotes Added</h3>
             <div class="progress">
                 <div class="progress">
-                    <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"
+                    <!-- <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" -->
+                     <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"
                     :style="{width: (quoteCount / maxQuotes) * 100 + '%'}">
                         {{ quoteCount }} / {{ maxQuotes }}
                     </div>
